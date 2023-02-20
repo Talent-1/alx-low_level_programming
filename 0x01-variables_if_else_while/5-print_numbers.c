a5824d1bfabe1f94b3e4ecc3eb7729d99840488b
root <talenttelecoms@gmail.com>
2023-02-20 15:56:53 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int v;
+
+	for (v = 0; v <= 9; v++)
+
+		printf("%d", v);
+
+	printf("\n");
+
+	return (0);
+}
