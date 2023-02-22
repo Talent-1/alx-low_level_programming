6b88547044d0b927570e8f2f2e6d4f6880839b44
root <talenttelecoms@gmail.com>
2023-02-22 05:39:14 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-times_table(3);
-_putchar('\n');
-times_table(5);
-_putchar('\n');
-times_table(98);
-_putchar('\n');
-times_table(12);
-_putchar('\n');
-return (0);
+	print_times_table(3);
+	-putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	_putchar('\n');
+	return (0);
 }
