99dfe194ef24b168acc06ab9593a841096e72a81
root <talenttelecoms@gmail.com>
2023-02-21 17:55:22 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet in lower case and a new line
+ * Return: always 0
+*/
+int main(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+		_putchar(k);
+
+	_putchar('\n');
+}
