7ed7edccb7822eff6d71350ff6277bfe8492d2cf
root <talenttelecoms@gmail.com>
2023-02-21 18:27:43 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printns alpha 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char k;
+
+	for (ten = '0'; ten < '10'; ten++)
+	{
+		for (k = 'a'; k <= 'z'; k++)
+			_putchar(k);
+
+		_putchar('\n');
+	}
+}
