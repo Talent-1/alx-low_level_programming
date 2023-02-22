e989909faeb1c957f6fabc10b36f350c1c10df01
root <talenttelecoms@gmail.com>
2023-02-22 01:08:10 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the value of an interger
+ *@t: the integer to be computed
+ *
+ * Return: absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
