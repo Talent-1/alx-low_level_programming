2cc6030bee36f32f65435d11008794f39f5d9ccb
root <talenttelecoms@gmail.com>
2023-02-23 16:51:13 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,8 +27,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("day of the year: %d\n", day);
-			printf("remaining days: %d\n", 365 - 0, day);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
