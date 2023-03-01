19d12a61d9ff78fe2d0d56d75fdd0f426e0e4b8e
root <talenttelecoms@gmail.com>
2023-03-01 14:23:11 -0800

    main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointert to an int as parameter
+ * and updates the value it points to to 98.
+ * @n: number to points to
+ *
+ * Return: viod
+ */
+int reset_to_98(int *n) 
+{
+	int n = 30;
+	*p = n;
+	_putchar("%d");
+	_putchar("*p");
+	_putchar('&');
+	putchar('n');
+
+	*p = 98;
+	_puchar("%d");
+	_putchar('n');
+	_putchar('\n');
+
+	return (void);
+}
