d57658d7cb2d740c6816c038433e62a32f53ed1d
root <talenttelecoms@gmail.com>
2023-03-02 13:28:51 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@ char s2[] = "World!\n";
 char *ptr;
 printf("%s\n", s1);
 printf("%s\n", s2);
-ptr = _strcat(s1, s2);
+ptr = _stract(s1, s2);
 printf("%s", s1);
 printf("%s", s2);
 printf("%s", ptr);
